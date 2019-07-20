a06a956afabbdfb6824c57ee3fe0fe88f9ed9ab2
Jacob Swanson <jswanson@covermymeds.com>
2019-07-20 18:37:05 -0400

    Add ability to log out

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -14,5 +14,11 @@ module SessionsHelper
 	# Returns true if the user is logged in, false otherwise.
   def logged_in?
     !current_user.nil?
+	end
+	
+	# Logs out the current user.
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
   end
 end
