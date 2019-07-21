12c894e770d74985634bbe65cfc8c2cb184c3938
Jacob Swanson <jswanson@covermymeds.com>
2019-07-21 17:28:09 -0400

    Add remember me button and logic

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_15_022625) do
+ActiveRecord::Schema.define(version: 2019_07_20_230238) do
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2019_07_15_022625) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
