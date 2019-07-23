7b4831da8ebbba6fd3bf11288b2f15deec1e76d6
Jacob Swanson <jswanson@covermymeds.com>
2019-07-23 16:50:37 -0400

    Add account activation. Update heroku production settings for mail

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,9 @@ User.create!(first_name: "Example",
 	email: "example@example.com",
 	password: "foobar",
 	password_confirmation: "foobar",
-	admin: true)
+	admin: true,
+	activated: true,
+	activated_at: Time.zone.now)
 
 99.times do |n|
 	first_name = Faker::Name.first_name
@@ -14,5 +16,7 @@ User.create!(first_name: "Example",
 		last_name: last_name,
 		email: email,
 		password: password,
-		password_confirmation: password)
+		password_confirmation: password,
+		activated: true,
+		activated_at: Time.zone.now)
 end
